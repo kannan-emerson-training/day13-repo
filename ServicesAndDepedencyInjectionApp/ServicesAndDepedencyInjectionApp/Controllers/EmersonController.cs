1b5a0b558dd9730296579125658be3775c60c1dd
kannan <kannans89@gmail.com>
2025-12-02 16:10:35 +0530

    view

---
+++ ServicesAndDepedencyInjectionApp/ServicesAndDepedencyInjectionApp/Controllers/EmersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServicesAndDepedencyInjectionApp.Controllers
+{
+    public class EmersonController: Controller
+    {
+
+        [HttpGet]
+        //public string Services()
+        //{
+        //    return "<h1>This is services page</h1>";
+        //}
+
+        public IActionResult Services()
+        {
+            //model
+            ViewBag.MyMessage = "services are on power and water";
+
+            return this.View();
+
+            
+        }
+    }
+}
--- ServicesAndDepedencyInjectionApp/ServicesAndDepedencyInjectionApp/Program.cs
+++ ServicesAndDepedencyInjectionApp/ServicesAndDepedencyInjectionApp/Program.cs
@@ -8,6 +8,7 @@ var app = builder.Build();
 
 //register middleware;
 
+//app.UseDefaultFiles();
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
 
